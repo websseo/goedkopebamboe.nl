bbd89b6ec25685b7477be34766990c0becdcec87
Websseo <contact@websmedia.nl>
2023-05-31 15:29:27 +0200

    Logo update

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,11 +1,10 @@
-/* eslint-disable no-process-env */
-import { loadEnvConfig } from "@next/env";
-import { defineCliConfig } from "sanity/cli";
+/**
+* This configuration file lets you run `$ sanity [command]` in this folder
+* Go to https://www.sanity.io/docs/cli to learn more.
+**/
+import { defineCliConfig } from 'sanity/cli'
 
-const dev = process.env.NODE_ENV !== "production";
-loadEnvConfig(__dirname, dev, { info: () => null, error: console.error });
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-
-export default defineCliConfig({ api: { projectId, dataset } });
+export default defineCliConfig({ api: { projectId, dataset } })
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,26 +1,25 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import { visionTool } from "@sanity/vision";
-import schemas from "./schemas/schemaTypes";
-import { Logo } from "./plugins/studioLogo";
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...index]]\page.tsx` route
+ */
 
-const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "[Studio] Commerce Layer Sanity Starter";
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schema'
 
 export default defineConfig({
-  title,
-  basePath: "/studio",
+  basePath: '/studio',
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool({ defaultApiVersion: apiVersion })],
-  schema: {
-    types: schemas
-  },
-  studio: {
-    components: {
-      logo: Logo
-    }
-  }
-});
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema,
+  plugins: [
+    deskTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
