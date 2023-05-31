36cc46db60f08fbf6815600bd62080ee80ccbc94
Websseo <contact@websmedia.nl>
2023-05-31 16:04:08 +0200

    Revert "Logo update"
    This reverts commit bbd89b6ec25685b7477be34766990c0becdcec87.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,25 +1,26 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...index]]\page.tsx` route
- */
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import schemas from "./schemas/schemaTypes";
+import { Logo } from "./plugins/studioLogo";
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "[Studio] Commerce Layer Sanity Starter";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
 
 export default defineConfig({
-  basePath: '/studio',
+  title,
+  basePath: "/studio",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
-  schema,
-  plugins: [
-    deskTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
-})
+  plugins: [deskTool(), visionTool({ defaultApiVersion: apiVersion })],
+  schema: {
+    types: schemas
+  },
+  studio: {
+    components: {
+      logo: Logo
+    }
+  }
+});
